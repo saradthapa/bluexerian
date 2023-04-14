f1d04a07f1620df3370af1fa9791b560ff09dbd1
Sarad Thapa <saradthapa@gmail.com>
2023-04-14 19:01:55 +0200

    Rolling back

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,36 +1,11 @@
 <script>
 	import '../app.css';
 	import Footer from '$lib/components/Footer.svelte';
-	import Header from '$lib/components/Header.svelte';
+	import Header from "$lib/components/Header.svelte";
 </script>
 
-<Header />
+<Header/>
 <section>
-	<slot />
+	<slot/>
 </section>
-<Footer />
-
-<style>
-	:global(body) {
-		background-image: url('img/bg-tablet-pattern.svg');
-		background-repeat: no-repeat;
-		background-size: 800px;
-		background-position: 90% -25%;
-	}
-
-	:global(#cta) {
-		background-image: url('img/bg-simplify-section-desktop.svg');
-		background-repeat: no-repeat;
-	}
-
-	@media (max-width: 576px) {
-		:global(body) {
-			background-position: 50px -50px;
-			background-size: 500px;
-		}
-
-		:global(#cta) {
-			background-image: url('img/bg-simplify-section-mobile.svg');
-		}
-	}
-</style>
+<Footer/>
